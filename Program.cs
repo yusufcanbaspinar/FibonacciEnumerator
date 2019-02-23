35c12805cea03028338b0ffacd803b599b240ba9
yusufcanbaspinar <40249072+yusufcanbaspinar@users.noreply.github.com>
2019-02-23 14:32:37 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace FibonacciEnumerator1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                FibonacciNumbers f = new FibonacciNumbers(200000);
+                foreach (int n in f)
+                {
+                    Console.WriteLine(n);
+                }
+                Console.ReadKey();
+        }
+    }
+    }
+    class FibonacciNumbers : IEnumerable
+    {
+        private FibonacciEnumerator iter;
+        public FibonacciNumbers(int max)
+        {
+            iter = new FibonacciEnumerator(max);
+        }
+        public IEnumerator GetEnumerator()
+        {
+            return iter;
+        }
+    }
+    class FibonacciEnumerator : IEnumerator
+    {
+        private int currentNumber, nextNumber, maxNumber;
+        public FibonacciEnumerator(int max)
+        {
+            maxNumber = max;
+            Reset();
+        }
+        public object Current { get { return currentNumber; } }
+        public bool MoveNext()
+        {
+            int newNextNumber = currentNumber + nextNumber;
+            currentNumber = nextNumber;
+            nextNumber = newNextNumber;
+
+            return currentNumber <= maxNumber;
+        }
+        public void Reset()
+        {
+            currentNumber = 0;
+            nextNumber = 1;
+        }
+    }
+
